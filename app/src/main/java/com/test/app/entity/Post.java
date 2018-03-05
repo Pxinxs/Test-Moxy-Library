fd832046502f14e2c21d5877bec6ebbfd1682b6d
Anton <pxinxs@gmail.com>
2018-03-05 19:53:29 +0200

    add comment in Post.java

--- app/src/main/java/com/test/app/entity/Post.java
+++ app/src/main/java/com/test/app/entity/Post.java
@@ -4,7 +4,7 @@ package com.test.app.entity;
  *Created by Anton on 15.02.2018.
  */
 public class Post {
-    public Integer userId;
+    public Integer userId;  // not used
     public Integer id;
     public String title;
     public String body;
